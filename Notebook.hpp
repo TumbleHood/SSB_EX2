6bd598100bfc49f0d33a84bbd15398842c5a6aca
TumbleHood <guyga8@gmail.com>
2022-03-22 17:51:29 +0200

    built class Notebook

---
+++ Notebook.hpp
@@ -0,0 +1,15 @@
+#include <string>
+
+using ariel::Direction;
+using namespace std;
+
+namespace ariel{
+    class Notebook{
+        public:
+            Notebook();
+            void write(unsigned int page, unsigned int row, unsigned int column, Direction direction, string text);
+            string read(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int length);
+            void erase(unsigned int page, unsigned int row, unsigned int column, Direction direction, unsigned int length);
+            void show(unsigned int page);
+    };
+}
