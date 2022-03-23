e102847b5f209fd2f1932ce80f4ea89917acd08e
TumbleHood <guyga8@gmail.com>
2022-03-23 10:06:58 +0200

    added more tests

--- Test.cpp
+++ Test.cpp
@@ -12,6 +12,8 @@ TEST_CASE("Good Input"){
     Direction h = Direction::Horizontal;
     Direction v = Direction::Vertical;
 
+    CHECK(nb.read(0, 0, 0, h, 10) == "__________");
+
     CHECK_NOTHROW(nb.write(0, 0, 0, h, "mind"));
     CHECK_NOTHROW(nb.write(0, 1, 0, h, "idea"));
     CHECK_NOTHROW(nb.write(0, 2, 0, h, "neat"));
@@ -26,19 +28,25 @@ TEST_CASE("Good Input"){
     CHECK_NOTHROW(nb.erase(0, 1, 3, v, 3));
     CHECK_NOTHROW(nb.erase(0, 2, 2, h, 1));
 
-    CHECK(nb.read(0, 0, 0, v, 4) == "m_nd");
-    CHECK(nb.read(0, 0, 1, v, 4) == "____");
-    CHECK(nb.read(0, 0, 2, v, 4) == "n___");
-    CHECK(nb.read(0, 0, 3, v, 4) == "d_t_");
+    CHECK(nb.read(0, 0, 0, v, 4) == "m~nd");
+    CHECK(nb.read(0, 0, 1, v, 4) == "~~~~");
+    CHECK(nb.read(0, 0, 2, v, 4) == "n~~~");
+    CHECK(nb.read(0, 0, 3, v, 4) == "d~t~");
 }
 
 TEST_CASE("Bad Input"){
     Notebook nb;
-    Direction d = Direction::Horizontal;
-    CHECK_THROWS(nb.write(1, 1, 101, d, "a"));
-    CHECK_THROWS(nb.write(1, 1, 95, d, "0123456789"));
-    CHECK_THROWS(nb.erase(1, 1, 222, d, 1));
-    CHECK_THROWS(nb.erase(1, 1, 94, d, 15));
-    CHECK_THROWS(nb.read(1, 1, 321, d, 1));
-    CHECK_THROWS(nb.read(1, 1, 50, d, 200));
+    Direction h = Direction::Horizontal;
+
+    CHECK_THROWS(nb.write(1, 1, 101, h, "a"));
+    CHECK_THROWS(nb.write(1, 1, 95, h, "0123456789"));
+    CHECK_THROWS(nb.erase(1, 1, 222, h, 1));
+    CHECK_THROWS(nb.erase(1, 1, 94, h, 15));
+    CHECK_THROWS(nb.read(1, 1, 321, h, 1));
+    CHECK_THROWS(nb.read(1, 1, 50, h, 200));
+
+    nb.write(0, 0, 0, h, "a");
+    CHECK_THROWS(nb.write(0, 0, 0, h, "a"));
+    nb.erase(0, 0, 0, h, 1);
+    CHECK_THROWS(nb.erase(0, 0, 0, h, 1));
 }
